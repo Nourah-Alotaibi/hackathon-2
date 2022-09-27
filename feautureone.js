0041823bc0db29cc17bce0e31de420e3629c2155
Nourah-Alotaibi <107700424+Nourah-Alotaibi@users.noreply.github.com>
2022-09-28 02:20:08 +0300

    For the love of the hackathon (Added Nora's files)

---
+++ feautureone.js
@@ -0,0 +1,28 @@
+const users = [
+    {id:  '1',
+    user: 'Norah',
+    civil_id: '123456',
+    number: '11223344',},
+    
+    {id:  '2',
+    user: 'Mona',
+    civil_id: '54321',
+    number: '1122334455',},
+    
+    {id:  '3',
+    user: 'Norhan',
+    civil_id: '1234567',
+    number: '1122334466',},
+    
+    {id:  '4',
+    user: 'Abdullah',
+    civil_id: '12345688',
+    number: '112233449',},
+    
+    {id:  '5',
+    user: 'Lana',
+    civil_id: '1234565',
+    number: '1122334455',},
+    
+    
+    ];
